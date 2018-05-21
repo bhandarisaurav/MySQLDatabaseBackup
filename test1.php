2926654262f6af6bd227e8ab214510747b601595
Saurav Bhandari <saurav.bhandari@deerwalk.edu.np>
2018-05-21 14:07:24 +0545

    bug fixes!!!

--- test1.php
+++
@@ -1,7 +0,0 @@
-<?php
-$backup_file = "Read.txt";
-$file_name = "README.md";
-$data = file_get_contents($file_name);
-header("Content-Disposition: attachment; filename='{$backup_file}'");
-echo $data;
-?>
